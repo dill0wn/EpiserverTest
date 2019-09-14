fab62926b6f8461cc0297dbf41403dfed149f054
dill0wn <wallsdil@gmail.com>
2019-09-14 11:03:56 -0400

    attempt at CMS shared block header; rolled it back

---
+++ DillonWallsC2Episerver/Models/ViewModels/GeneralPageModel.cs
@@ -0,0 +1,26 @@
+﻿using DillonWallsC2Episerver.Models.Blocks;
+using EPiServer.Core;
+using EPiServer.Web;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DillonWallsC2Episerver.Models.ViewModels
+{
+    public class GeneralPageModel
+    {
+        public HeaderBlock Header { get; set; }
+
+        [UIHint(UIHint.Image)]
+        public ContentReference BannerImage { get; set; }
+
+        public String Title { get; set; }
+
+        public String SubTitle { get; set; }
+
+        public XhtmlString MainBody { get; set; }
+
+    }
+}
